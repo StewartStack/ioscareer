b4d280e302facc10edf1f1375b7db2473762dbc9
John Stewart <jstewartstack@gmail.com>
2022-04-23 17:30:37 +0100

    Initial Commit

---
+++ getswiftyhistory/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  getswiftyhistory
+//
+//  Created by John Stewart on 22/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    var body: some View {
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("What was the first computer Bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack {
+                    Button(action: {
+                            print("Tapped on Choice 1")
+                    }, label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                            print("Tapped on Choice 2")
+                    }, label: {
+                        Text("Bettle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                            print("Tapped on Choice 3")
+                    }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                            print("Tapped on Choice 4")
+                    }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                }
+            }
+        }
+        .foregroundColor(Color.white)
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ getswiftyhistory/getswiftyhistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  getswiftyhistoryApp.swift
+//  getswiftyhistory
+//
+//  Created by John Stewart on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct getswiftyhistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
